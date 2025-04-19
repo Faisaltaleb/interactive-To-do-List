d7f0b77e5bfc0d93ea9b07dc7125dd6255c97df5
faisal <talebfaisal1@gmail.com>
2025-04-19 15:19:58 +0300

    intialized javascript file with basic setup

---
+++ script.js
@@ -0,0 +1,7 @@
+const taskInput =document.getElementById('taskInput');
+const addTaskBtn = document.getElementById('addTaskBtn');
+const taskList = document.getElementById('taskList');
+const filters = document.querySelector('.filters');
+
+let tasks = [];
+
